092025d1977494fbe96dd38e03f682150be2ec30
Alejandro Sanchez <alejandro862003@gmail.com>
2023-05-06 21:53:28 -0300

    modificando linea

--- js/js.js
+++ js/js.js
@@ -25,8 +25,8 @@ console.log(ingresarEdad);
 /*
 let num1 = parseInt(prompt("Ingrese el primer numero"));
 let num2 = parseInt(prompt("Ingrese el segundo numero"));
-let resultado = num1 + num2;
-alert("El resultado de la suma es: " + resultado);
+let resultado = num1 - num2;
+alert("El resultado de la resta es: " + resultado);
 */
 
 alert("Aprete el boton para realizar la suma");
